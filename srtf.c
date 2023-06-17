cabbb0891c0e3bcfbcb0962c6995506f91117c82
Abhay Kesarwani <103197529+abhayKesarwa@users.noreply.github.com>
2023-06-17 22:28:55 +0530

    Update srtf.c

--- srtf.c
+++ srtf.c
@@ -33,11 +33,8 @@ void main(){
             }
         }
 
-        printf("min_br: %d",min_br);  
-        
-
         for(int x=0;x<n;x++){
-            if(min_br==p[3][x] && time>=p[1][x] && p[3][x]<0){
+            if(min_br==p[3][x] && time>=p[1][x] && p[3][x]>0){
                 time_arr[time]=p[0][x];
                 p[3][x]=p[3][x]-1;
                 time++;
@@ -45,67 +42,38 @@ void main(){
                 break;
             }
         }
+        min_br++;
     }
     for(int i=0;i<time;i++){
         printf("%d ",time_arr[i]);
     }
 
-    // printf("Gantt Chart :\n");
-
-    // while(time_process_count!=0){
-
-    //     temp=stack[stack_out];
-    //     for(int x=0;x<q;x++){
-    //         if(p[3][temp-1]!=0){
-    //             time_arr[time]=stack[stack_out];
-    //             p[3][temp-1]=p[3][temp-1]-1;
-    //             time++;
-    //             time_process_count=time_process_count-1;
-    //             printf("%d ",p[0][temp-1]);
-    //         }
-    //     }
-    //     printf(" | ");
-    //     for(int x=0;x<n;x++){
-    //         if(time>=p[1][x] && p[5][x]==1){
-    //             stack[stack_count]=p[0][x];
-    //             p[5][x]=0;
-    //             stack_count++;
-    //         }
-    //     }
-    //     stack_out++;
-        
-    //     if(p[3][temp-1]!=0){
-    //         stack[stack_count]=p[0][temp-1];
-    //         stack_count++;
-    //     }
-    // }
-
     //calculating waiting time
-    // int process_count=0,y;
-    // for(int x=0;x<n;x++){
-    //     process_count=0;
-    //     for(y=time_process_count_copy-1;y>=0;y--){
-    //         if(x+1==time_arr[y]){
-    //             if(time_arr[y]==time_arr[y-1]){
-    //                 continue;
-    //             }
-    //             for(int z=y-1;z>=0;z--){
-    //                 if(time_arr[z]==x+1){
-    //                     process_count++;
-    //                 }
-    //             }
-    //             break;
-    //         }
+    int process_count=0,y;
+    for(int x=0;x<n;x++){
+        process_count=0;
+        for(y=time_process_count_copy-1;y>=0;y--){
+            if(x+1==time_arr[y]){
+                if(time_arr[y]==time_arr[y-1]){
+                    continue;
+                }
+                for(int z=y-1;z>=0;z--){
+                    if(time_arr[z]==x+1){
+                        process_count++;
+                    }
+                }
+                break;
+            }
             
-    //     }
-    //     p[3][x]=y-process_count-p[1][x];
-    //     //calculating turnarround time
-    //     p[4][x]=p[2][x]+p[3][x];
-    // }
+        }
+        p[3][x]=y-process_count-p[1][x];
+        //calculating turnarround time
+        p[4][x]=p[2][x]+p[3][x];
+    }
     
-    // printf("\nProcess ID   Arrival time   Burst time   Waiting time   Turnaround time\n");
-    // for(int i=0;i<n;i++){
-    //     printf("P %d\t\t%d\t\t%d\t\t%d\t\t%d\n",p[0][i],p[1][i],p[2][i],p[3][i],p[4][i]);
+    printf("\nProcess ID   Arrival time   Burst time   Waiting time   Turnaround time\n");
+    for(int i=0;i<n;i++){
+        printf("P %d\t\t%d\t\t%d\t\t%d\t\t%d\n",p[0][i],p[1][i],p[2][i],p[3][i],p[4][i]);
 
-    // }
+    }
 }
